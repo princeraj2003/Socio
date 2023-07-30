83389e24a4b69f2e9162712656c4ccb9d23bb993
princeraj2003 <126746978+princeraj2003@users.noreply.github.com>
2023-07-30 09:49:17 +0530

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -262,7 +262,7 @@ const PostWidget = ({
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-    const response = await fetch(`${process.env.REACT_URL}/posts/${postId}/like`, {
+     const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
