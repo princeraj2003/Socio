1c925d239700ba0933310eae217de8aad6422d5e
princeraj2003 <126746978+princeraj2003@users.noreply.github.com>
2023-09-08 18:01:54 +0530

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -262,7 +262,7 @@ const PostWidget = ({
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-     const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+     const response = await fetch(`https://socially-8ulx.onrender.com/posts/${postId}/like`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -291,7 +291,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`${process.env.REACT_URL}/assets/${picturePath}`}
+          src={`https://socially-8ulx.onrender.com/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
