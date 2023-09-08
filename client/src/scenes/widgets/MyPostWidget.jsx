f5c32c35b6507e89800948be718b9038b494fbc2
princeraj2003 <126746978+princeraj2003@users.noreply.github.com>
2023-09-08 18:01:07 +0530

    Update MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -572,7 +572,7 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picture", image);
       formData.append("picturePath", image.name);
     }
-const response = await fetch(`http://localhost:3001/posts`, {
+const response = await fetch(`https://socially-8ulx.onrender.com/posts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
