cce98a208f5c7607e0af2828404f774bd9fa6398
princeraj2003 <126746978+princeraj2003@users.noreply.github.com>
2023-09-08 17:58:59 +0530

    Update index.jsx

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -80,7 +80,7 @@ const ProfilePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
   const getUser = async () => {
-   const response = await fetch(`http://localhost:3001/users/${userId}`, {
+   const response = await fetch(`https://socially-8ulx.onrender.com/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
